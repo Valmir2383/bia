24a2bdb511418b7d3dcbb3bbc24e99a2eba7114a
valmir <valmir.s.s.jr2383@gmail.com>
2026-02-27 00:04:48 +0000

    botão

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar nova Task
+        Add Task
       </button>
       
       <Modal
