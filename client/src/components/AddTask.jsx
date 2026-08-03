7e73d1c81a0aa0aa3de2b3ffbf80fe2ac5444192
Valmir Souza <valmir.s.s.jr2383@gmail.com>
2026-08-03 16:19:57 +0000

    ajuste para alb

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar tarefa com IA
+       Adicionar Task com ALB
       </button>
       
       <Modal
